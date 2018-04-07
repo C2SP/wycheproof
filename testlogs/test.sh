821c447fcc7b6ae26a3bf3adeaa816e28bfe2d7c
Tink Team <noreply@google.com>
2018-04-07 01:39:51 -0400

    Testlogs auto-pushed to gh-pages

---
+++ testlogs/test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+generate_index_html() {
+  testlogs_dir=$1
+
+  outfile=$testlogs_dir/index.html
+  echo "<html><body><ul>" >> $outfile
+  for f in `ls $testlogs_dir | egrep '(txt)'`; do
+    report=$(grep 'Tests run: [0-9]\{1,\},  Failures: [0-9]\{1,\}' $testlogs_dir/$f)
+    test_name=$f
+    if [[ $f = *"BouncyCastle"* ]]; then
+      test_name="SpongyCastle/$f"
+    fi
+    echo "<li><a href=\"$f\">$test_name</a>: $report</li>" >> $outfile;
+  done;
+  echo "</ul></body></html>" >> $outfile;
+}
+
+generate_index_html .
