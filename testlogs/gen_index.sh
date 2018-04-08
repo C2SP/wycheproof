ed03e7a0d21d613c14997ebdae854499c4d67810
Tink Team <noreply@google.com>
2018-04-07 20:14:20 -0400

    Testlogs auto-pushed to gh-pages

--- testlogs/gen_index.sh
+++ testlogs/gen_index.sh
@@ -4,7 +4,8 @@ generate_index_html() {
   testlogs_dir=$1
 
   outfile=$testlogs_dir/index.html
-  echo "<html><body><ul>" >> $outfile
+  echo "<html><body>i<p>Last update: $(date)</p>" > $outfile
+  echo "<p><ul>" >> $outfile
   for f in `ls $testlogs_dir | egrep '(txt)'`; do
     report=$(grep 'Tests run: [0-9]\{1,\},  Failures: [0-9]\{1,\}' $testlogs_dir/$f)
     test_name=$f
@@ -13,7 +14,7 @@ generate_index_html() {
     fi
     echo "<li><a href=\"$f\">$test_name</a>: $report</li>" >> $outfile;
   done;
-  echo "</ul></body></html>" >> $outfile;
+  echo "</ul></p></body></html>" >> $outfile;
 }
 
 generate_index_html .
