9a76220d11dfee7708b85228952c0e68e50c5854
Wycheproof Team <noreply@google.com>
2020-11-02 19:01:00 -0800

    Add missing `goog.require`/`goog.requireType` statements
    A "missing require" is when a JS file uses a namespace implicitly:
    
    ```
    // no import of 'foo.bar.baz'
    /** @type {foo.bar.baz} */
    ```
    
    without first importing that namespace with a `goog.require` (or `goog.requireType`), like:
    
    ```
    goog.requireType('foo.bar.baz');
    /** @type {foo.bar.baz} */
    ```
    
    We are changing the compiler to forbid "missing requires" consistently, and
    this LSC is cleaning up existing code that violates this rule.
    
    LSC doc: go/lsc-missing-requires
    
    NOKEYCHECK=True
    PiperOrigin-RevId: 340360814

--- javascript/TestUtil.js
+++ javascript/TestUtil.js
@@ -21,10 +21,11 @@ goog.provide('wycheproof.TestUtil');
 goog.require('goog.crypt');
 goog.require('goog.crypt.base64');
 goog.require('goog.json');
+goog.requireType('goog.testing.TestCase.Result');
 
 /**
  * Reads test vectors in JSON format from a file.
- * @param {!string} filename
+ * @param {string} filename
  *
  * @return {!JSONObject}
  */
@@ -53,9 +54,9 @@ wycheproof.TestUtil.checkTestCaseResult = function(result) {
 
 /**
  * Checks whether the given string is in hex format.
- * @param {!string} s
+ * @param {string} s
  *
- * @return {!boolean}
+ * @return {boolean}
  */
 wycheproof.TestUtil.isHex = function(s) {
   return /(^[0-9A-F]*$)|(^[0-9a-f]*$)/.test(s);
@@ -63,7 +64,7 @@ wycheproof.TestUtil.isHex = function(s) {
 
 /**
  * Converts a hex string to a ArrayBuffer.
- * @param {!string} s
+ * @param {string} s
  *
  * @return {!ArrayBuffer}
  */
@@ -76,7 +77,7 @@ wycheproof.TestUtil.hexToArrayBuffer = function(s) {
  * Converts an ArrayBuffer to hex string
  * @param {!ArrayBuffer} ab
  *
- * @return {!string}
+ * @return {string}
  */
 wycheproof.TestUtil.arrayBufferToHex = function(ab) {
   return goog.crypt.byteArrayToHex(new Uint8Array(ab));
@@ -84,9 +85,9 @@ wycheproof.TestUtil.arrayBufferToHex = function(ab) {
 
 /**
  * Converts a Base64URL string to hex format
- * @param {!string} s
+ * @param {string} s
  *
- * @return {!string}
+ * @return {string}
  */
 wycheproof.TestUtil.base64UrlToHex = function (s) {
   var b64str = wycheproof.TestUtil.base64UrlToBase64(s);
@@ -95,9 +96,9 @@ wycheproof.TestUtil.base64UrlToHex = function (s) {
 
 /**
  * Converts a hex string to Base64 format.
- * @param {!string} s
+ * @param {string} s
  *
- * @return {!string}
+ * @return {string}
  */
 wycheproof.TestUtil.hexToBase64 = function(s) {
   var bytes = goog.crypt.hexToByteArray(s);
@@ -106,9 +107,9 @@ wycheproof.TestUtil.hexToBase64 = function(s) {
 
 /**
  * Converts a base64 string to hex format.
- * @param {!string} s
+ * @param {string} s
  *
- * @return {!string}
+ * @return {string}
  */
 wycheproof.TestUtil.base64ToHex = function(s) {
   var bytes = goog.crypt.base64.decodeStringToByteArray(s);
@@ -117,9 +118,9 @@ wycheproof.TestUtil.base64ToHex = function(s) {
 
 /**
  * Converts a Base64 string to Base64URL
- * @param {!string} s
+ * @param {string} s
  *
- * @return {!string}
+ * @return {string}
  */
 wycheproof.TestUtil.base64ToBase64Url = function(s) {
   return s.replace(/\+/g, '-')
@@ -129,9 +130,9 @@ wycheproof.TestUtil.base64ToBase64Url = function(s) {
 
 /**
  * Converts a Base64Url string to Base64
- * @param {!string} s
+ * @param {string} s
  *
- * @return {!string}
+ * @return {string}
  */
 wycheproof.TestUtil.base64UrlToBase64 = function(s) {
   return (s + '==='.slice((s.length + 3) % 4))
