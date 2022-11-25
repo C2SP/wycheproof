3649ec1f111e34645bbae0af63508e4e41d4aa46
bleichen <bleichen@google.com>
2022-11-25 02:43:39 -0800

    Refactoring JsonCipherTest.java.
    The tests now use an instance of TestResult instead of writing to System.out.
    This makes it easier for third parties to run the tests.
    
    NOKEYCHECK=True
    PiperOrigin-RevId: 490865326

--- java/com/google/security/wycheproof/testcases/JsonCipherTest.java
+++ java/com/google/security/wycheproof/testcases/JsonCipherTest.java
@@ -14,7 +14,6 @@
 package com.google.security.wycheproof;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -23,9 +22,10 @@ import com.google.security.wycheproof.WycheproofRunner.NoPresubmitTest;
 import com.google.security.wycheproof.WycheproofRunner.ProviderType;
 import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Set;
-import java.util.TreeSet;
+import java.security.spec.AlgorithmParameterSpec;
+import java.util.Arrays;
 import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import org.junit.Test;
@@ -43,40 +43,55 @@ import org.junit.runners.JUnit4;
 public class JsonCipherTest {
 
   /** Convenience method to get a byte array from a JsonObject. */
-  protected static byte[] getBytes(JsonObject object, String name) throws Exception {
+  protected static byte[] getBytes(JsonObject object, String name) {
     return JsonUtil.asByteArray(object.get(name));
   }
 
-  protected static boolean arrayEquals(byte[] a, byte[] b) {
-    if (a.length != b.length) {
-      return false;
+  protected static Cipher getCipher(String algorithm)
+      throws NoSuchAlgorithmException, NoSuchPaddingException {
+    switch (algorithm) {
+      case "AES-CBC-PKCS5":
+        return Cipher.getInstance("AES/CBC/PKCS5Padding");
+      case "ARIA-CBC-PKCS5":
+        return Cipher.getInstance("ARIA/CBC/PKCS5Padding");
+      case "CAMELLIA-CBC-PKCS5":
+        return Cipher.getInstance("CAMELLIA/CBC/PKCS5Padding");
+      default:
+        throw new NoSuchAlgorithmException("Unsupported algorithm:" + algorithm);
     }
-    byte res = 0;
-    for (int i = 0; i < a.length; i++) {
-      res |= (byte) (a[i] ^ b[i]);
-    }
-    return res == 0;
   }
 
   /**
-   * Initialize a Cipher instance.
+   * Returns an initialized Cipher instance.
    *
-   * @param cipher an instance of a symmetric cipher that will be initialized.
    * @param algorithm the name of the algorithm used (e.g. "AES/CBC/PKCS5Padding")
    * @param opmode either Cipher.ENCRYPT_MODE or Cipher.DECRYPT_MODE
    * @param key raw key bytes
    * @param iv the initialisation vector
    */
-  protected static void initCipher(
-      Cipher cipher, String algorithm, int opmode, byte[] key, byte[] iv) throws Exception {
-    SecretKeySpec keySpec = null;
-    if (algorithm.startsWith("AES/")) {
-      keySpec = new SecretKeySpec(key, "AES");
-    } else {
-      fail("Unsupported algorithm:" + algorithm);
+  protected static Cipher getInitializedCipher(String algorithm, int opmode, byte[] key, byte[] iv)
+      throws GeneralSecurityException {
+    Cipher cipher = getCipher(algorithm);
+    SecretKeySpec keySpec;
+    AlgorithmParameterSpec parameters;
+    switch (algorithm) {
+      case "AES-CBC-PKCS5":
+        keySpec = new SecretKeySpec(key, "AES");
+        parameters = new IvParameterSpec(iv);
+        break;
+      case "ARIA-CBC-PKCS5":
+        keySpec = new SecretKeySpec(key, "ARIA");
+        parameters = new IvParameterSpec(iv);
+        break;
+      case "CAMELLIA-CBC-PKCS5":
+        keySpec = new SecretKeySpec(key, "CAMELLIA");
+        parameters = new IvParameterSpec(iv);
+        break;
+      default:
+        throw new NoSuchAlgorithmException("Unsupported algorithm:" + algorithm);
     }
-    IvParameterSpec ivSpec = new IvParameterSpec(iv);
-    cipher.init(opmode, keySpec, ivSpec);
+    cipher.init(opmode, keySpec, parameters);
+    return cipher;
   }
 
   /**
@@ -119,145 +134,167 @@ public class JsonCipherTest {
    *       ...
    * </pre>
    */
-  // This is a false positive, since errorprone cannot track values passed into a method.
-  @SuppressWarnings("InsecureCryptoUsage")
-  public void testCipher(String filename, String algorithm) throws Exception {
-    JsonObject test = JsonUtil.getTestVectorsV1(filename);
-    String schema = test.get("schema").getAsString();
-    String expectedSchema = "ind_cpa_test_schema.json";
-    if (!schema.equals(expectedSchema)) {
-      System.out.println(
-          algorithm
-              + ": expecting test vectors with schema "
-              + expectedSchema
-              + " found "
-              + schema);
-    }
-    Set<String> exceptions = new TreeSet<>();
-    int numTests = test.get("numberOfTests").getAsInt();
-    int cntTests = 0;
-    int errors = 0;
+  private static void singleTest(String algorithm, JsonObject testcase, TestResult testResult) {
+    int tcId = testcase.get("tcId").getAsInt();
+    byte[] key = getBytes(testcase, "key");
+    byte[] iv = getBytes(testcase, "iv");
+    byte[] msg = getBytes(testcase, "msg");
+    byte[] ciphertext = getBytes(testcase, "ct");
+    // Result is one of "valid", "invalid", "acceptable".
+    // "valid" are test vectors with matching plaintext, ciphertext and tag.
+    // "invalid" are test vectors with invalid parameters or invalid ciphertext and tag.
+    // "acceptable" are test vectors with weak parameters or legacy formats.
+    String result = testcase.get("result").getAsString();
+
+    // Test encryption
     Cipher cipher;
     try {
-      cipher = Cipher.getInstance(algorithm);
-    } catch (NoSuchAlgorithmException ex) {
-      System.out.println("Algorithm is not supported. Skipping test for " + algorithm);
+      cipher = getInitializedCipher(algorithm, Cipher.ENCRYPT_MODE, key, iv);
+    } catch (GeneralSecurityException ex) {
+      // Some libraries restrict key size, iv size and tag size.
+      // Because of the initialization of the cipher might fail.
+      testResult.addResult(tcId, TestResult.Type.REJECTED_ALGORITHM, ex.toString());
       return;
+    }
+    TestResult.Type resultType;
+    String comment = "";
+    // Normally the test tries to encrypt and decrypt a ciphertext.
+    // tryDecrypt is set to false if a bug during encryption was serious enough,
+    // so that trying to decrypt no longer makes sense.
+    boolean tryDecrypt = true;
+    try {
+      byte[] encrypted = cipher.doFinal(msg);
+      boolean eq = Arrays.equals(ciphertext, encrypted);
+      if (result.equals("invalid")) {
+        if (eq) {
+          // Some test vectors use invalid parameters that should be rejected.
+          // E.g. an implementation must never encrypt using AES-GCM with an IV of length 0,
+          // since this leaks the authentication key.
+          resultType = TestResult.Type.NOT_REJECTED_INVALID;
+          tryDecrypt = false;
+        } else {
+          // Invalid test vectors frequently have invalid tags.
+          // Hence encryption just gives a different result.
+          resultType = TestResult.Type.REJECTED_INVALID;
+        }
+      } else {
+        if (!eq) {
+          // If encryption returns the wrong result then something is
+          // broken. Hence we can stop here.
+          resultType = TestResult.Type.WRONG_RESULT;
+          comment = "ciphertext: " + TestUtil.bytesToHex(encrypted);
+          tryDecrypt = false;
+        } else {
+          resultType = TestResult.Type.PASSED_VALID;
+        }
+      }
+    } catch (GeneralSecurityException ex) {
+      if (result.equals("valid")) {
+        resultType = TestResult.Type.REJECTED_VALID;
+      } else {
+        resultType = TestResult.Type.REJECTED_INVALID;
+      }
+    }
+
+    if (tryDecrypt) {
+      // Test decryption
+      try {
+        Cipher decCipher = getInitializedCipher(algorithm, Cipher.DECRYPT_MODE, key, iv);
+        byte[] decrypted = decCipher.doFinal(ciphertext);
+        boolean eq = Arrays.equals(decrypted, msg);
+        if (result.equals("invalid")) {
+          resultType = TestResult.Type.NOT_REJECTED_INVALID;
+        } else if (!eq) {
+          resultType = TestResult.Type.WRONG_RESULT;
+          comment = "decrypted:" + TestUtil.bytesToHex(decrypted);
+        } else {
+          resultType = TestResult.Type.PASSED_VALID;
+        }
+      } catch (GeneralSecurityException ex) {
+        comment = ex.toString();
+        if (result.equals("valid")) {
+          resultType = TestResult.Type.REJECTED_VALID;
+        } else {
+          resultType = TestResult.Type.REJECTED_INVALID;
+        }
+      }
+    }
+    testResult.addResult(tcId, resultType, comment);
+  }
+
+  /**
+   * Checks each test vector in a file of test vectors.
+   *
+   * <p>One motivation for running all the test vectors in a file at once is that this allows us to
+   * test if invalid paddings result in distinguishable exceptions. Throwing distinguishable
+   * exceptions can contain information that helps an attacker in a chosen ciphertext attack.
+   *
+   * @param testVectors the test vectors
+   * @return a test result
+   */
+  public static TestResult allTests(TestVectors testVectors) {
+    var testResult = new TestResult(testVectors);
+    JsonObject test = testVectors.getTest();
+    String algorithm = test.get("algorithm").getAsString();
+    try {
+      Cipher unused = getCipher(algorithm);
+    } catch (NoSuchAlgorithmException | NoSuchPaddingException ex) {
+      testResult.addFailure(TestResult.Type.REJECTED_ALGORITHM, algorithm);
+      return testResult;
     }
     for (JsonElement g : test.getAsJsonArray("testGroups")) {
       JsonObject group = g.getAsJsonObject();
       for (JsonElement t : group.getAsJsonArray("tests")) {
-        cntTests++;
         JsonObject testcase = t.getAsJsonObject();
-        int tcid = testcase.get("tcId").getAsInt();
-        String tc = "tcId: " + tcid + " " + testcase.get("comment").getAsString();
-        byte[] key = getBytes(testcase, "key");
-        byte[] iv = getBytes(testcase, "iv");
-        byte[] msg = getBytes(testcase, "msg");
-        byte[] ciphertext = getBytes(testcase, "ct");
-        // Result is one of "valid", "invalid", "acceptable".
-        // "valid" are test vectors with matching plaintext, ciphertext and tag.
-        // "invalid" are test vectors with invalid parameters or invalid ciphertext and tag.
-        // "acceptable" are test vectors with weak parameters or legacy formats.
-        String result = testcase.get("result").getAsString();
-
-        // Test encryption
-        try {
-          initCipher(cipher, algorithm, Cipher.ENCRYPT_MODE, key, iv);
-        } catch (GeneralSecurityException ex) {
-          // Some libraries restrict key size, iv size and tag size.
-          // Because of the initialization of the cipher might fail.
-          System.out.println(ex.toString());
-          continue;
-        }
-        try {
-          byte[] encrypted = cipher.doFinal(msg);
-          boolean eq = arrayEquals(ciphertext, encrypted);
-          if (result.equals("invalid")) {
-            if (eq) {
-              // Some test vectors use invalid parameters that should be rejected.
-              System.out.println("Encrypted " + tc);
-              errors++;
-            }
-          } else {
-            if (!eq) {
-              System.out.println(
-                  "Incorrect ciphertext for "
-                      + tc
-                      + " ciphertext:"
-                      + TestUtil.bytesToHex(encrypted));
-              errors++;
-            }
-          }
-        } catch (GeneralSecurityException ex) {
-          if (result.equals("valid")) {
-            System.out.println("Failed to encrypt " + tc);
-            errors++;
-          }
-        }
-
-        // Test decryption
-        // The algorithms tested in this class are typically malleable. Hence, it is in possible
-        // that modifying ciphertext randomly results in some other valid ciphertext.
-        // However, all the test vectors in Wycheproof are constructed such that they have
-        // invalid padding. If this changes then the test below is too strict.
-        try {
-          initCipher(cipher, algorithm, Cipher.DECRYPT_MODE, key, iv);
-        } catch (GeneralSecurityException ex) {
-          System.out.println("Parameters accepted for encryption but not decryption " + tc);
-          errors++;
-          continue;
-        }
-        try {
-          byte[] decrypted = cipher.doFinal(ciphertext);
-          boolean eq = arrayEquals(decrypted, msg);
-          if (result.equals("invalid")) {
-            System.out.println("Decrypted invalid ciphertext " + tc + " eq:" + eq);
-            errors++;
-          } else {
-            if (!eq) {
-              System.out.println(
-                  "Incorrect decryption " + tc + " decrypted:" + TestUtil.bytesToHex(decrypted));
-            }
-          }
-        } catch (GeneralSecurityException ex) {
-          exceptions.add(ex.getMessage());
-          if (result.equals("valid")) {
-            System.out.println("Failed to decrypt " + tc);
-            errors++;
-          }
-        }
+        singleTest(algorithm, testcase, testResult);
       }
     }
-    assertEquals(0, errors);
-    assertEquals(numTests, cntTests);
-    // Generally it is preferable if trying to decrypt ciphertexts with incorrect paddings
-    // does not leak information about invalid paddings through exceptions.
-    // Such information could simplify padding attacks. Ideally, providers should not include
-    // any distinguishing features in the exception. Hence, we expect just one exception among
-    // all test vectors that have the flag BadPadding. Using the class TestResult, will
-    // support such a test. Exceptions for other test vectors (e.g., ciphertexts with a size
-    // that are not a multiple of the block size) need not be indistinguishable.
-    //
-    // Seeing distinguishable exceptions, doesn't necessarily mean that protocols using
-    // the cipher with the tested provider are vulnerable to attacks. Rather it means
-    // that the provider might simplify attacks if an implementation is careless.
-    System.out.println("Number of distinct exceptions:" + exceptions.size());
-    for (String ex : exceptions) {
-      System.out.println(ex);
+    // Test vectors with invalid padding must have indistinguishable behavior.
+    // The test here checks for distinct exceptions. There are other ways to
+    // distinguish paddings, such as timing differences. Such differences are
+    // not checked here.
+    // Some invalid paddings are excluded here: E.g., BouncyCastle throws
+    // IllegalBlockSize exceptions if the ciphertext is empty. This exception
+    // is of course distinguishable from BadPaddingExceptions. It can be excluded
+    // since it does not leak information. Such test vectors do not include
+    // a "BadPadding" flag.
+    testResult.checkIndistinguishableResult("BadPadding");
+    return testResult;
+  }
+
+  public void testCipher(String filename) throws Exception {
+    JsonObject test = JsonUtil.getTestVectorsV1(filename);
+    TestVectors testVectors = new TestVectors(test, filename);
+    TestResult testResult = allTests(testVectors);
+
+    if (testResult.skipTest()) {
+      System.out.println("Skipping " + filename + " no ciphertext decrypted.");
+      TestUtil.skipTest("No ciphertext decrypted");
+      return;
     }
+    System.out.print(testResult.asString());
+    assertEquals(0, testResult.errors());
   }
 
   // jdk11 accepts an empty ciphertext.
   @NoPresubmitTest(
-      providers = {ProviderType.OPENJDK},
+      providers = {ProviderType.OPENJDK, ProviderType.CONSCRYPT},
       bugs = {"b/258666069"})
   @ExcludedTest(
-    providers = {ProviderType.CONSCRYPT},
-    comment = "Conscrypt accepts empyt ciphertexts."
-  )
+      providers = {ProviderType.CONSCRYPT},
+      comment = "Conscrypt accepts empyt ciphertexts.")
   @Test
   public void testAesCbcPkcs5() throws Exception {
-    testCipher("aes_cbc_pkcs5_test.json", "AES/CBC/PKCS5Padding");
+    testCipher("aes_cbc_pkcs5_test.json");
+  }
+
+  @Test
+  public void testAriaCbcPkcs5() throws Exception {
+    testCipher("aria_cbc_pkcs5_test.json");
+  }
+
+  @Test
+  public void testCamelliaCbcPkcs5() throws Exception {
+    testCipher("camellia_cbc_pkcs5_test.json");
   }
 }
