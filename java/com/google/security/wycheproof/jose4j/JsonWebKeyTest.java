8dab445c90e213696e232127a0fc059185bdb6fb
bleichen <bleichen@google.com>
2023-01-30 08:49:08 -0800

    Adding more test vector for JWK.
    NOKEYCHECK=True
    PiperOrigin-RevId: 505705966

--- java/com/google/security/wycheproof/jose4j/JsonWebKeyTest.java
+++ java/com/google/security/wycheproof/jose4j/JsonWebKeyTest.java
@@ -15,11 +15,11 @@ import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
 import com.google.common.flogger.GoogleLogger;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.security.wycheproof.JsonUtil;
+import com.google.security.wycheproof.TestUtil;
 import com.google.testing.testsize.MediumTest;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,7 +34,19 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
-/** Tests for <a href="https://tools.ietf.org/html/rfc7517">JSON Web Key RFC</a> */
+/**
+ * Tests for <a href="https://tools.ietf.org/html/rfc7517">JSON Web Key RFC</a>
+ *
+ * <p>The test checks key validation in jose4j. For example the following issues
+ * are tested:
+ *
+ * <ul>
+ *   <li>The key size of RSA keys must be at least 2048 bits. Specified in RFC 7518
+ *   <li>e must not be 1. This is totally insecure but did happend before (CVE-2011-4121)
+ *   <li>It would be nice if ROCA keys were rejected.
+ *   <li>The key size of HMAC keys must be at least as big as the HMAC digest.
+ * </ul>
+ */
 @MediumTest
 @RunWith(Parameterized.class)
 public class JsonWebKeyTest {
@@ -46,10 +58,20 @@ public class JsonWebKeyTest {
   private ImmutableSet<String> getSuppressedTests() {
     return ImmutableSet.of(
         // jose.4.j doesn't care if you mix inappropriate keys.
-        // TODO(bleichen): Typically the things to test should be in the test and not
-        //   in the test group. This test vector contains an strange key set. Hence,
-        //   this test would be better in a test for key set validations.
-        "jws_mixedSymmetryKeyset_rejectsValid_tcId1");
+        "jws_mixedSymmetryKeyset_rejectsValid_tcId1",
+        // TODO(bleichen): determine if there is an expected behavior for keysets
+        //   with duplicate keys.
+        // RFC 7517 Section 4.5 defines kids. It says that kids should be distinct,
+        // but that keysets with duplicate kids are acceptable if the keys can
+        // be distinguished by "kty".
+        // This test vector contains two keys with the same kid. The first key
+        // is valid an verifies the MAC.
+        "jws_duplicate_kid_rejectsDuplicateKid_tcId4",
+        // jose.4.j does some simple key validation. For example it rejects
+        // 1024 bit RSA keys and keys with public exponent e = 1.
+        // This test vector contains an RSA key with the ROCA vulnerability.
+        // Nothing checks for such weak keys during the verification process.
+        "jws_rsa_roca_key_rejectsKeyWithRocaVulnerability_tcId7");
   }
 
   /** A JsonWebCryptoTestGroup that contains key information and tests against those keys. */
@@ -64,7 +86,7 @@ public class JsonWebKeyTest {
   public String testName;
 
   @Parameters(name = "{2}")
-  public static Iterable<Object[]> produceTestCases() throws Exception {
+  public static List<Object[]> produceTestCases() throws Exception {
     JsonObject test = JsonUtil.getTestVectors("json_web_key_test.json");
 
     // Generate test cases.
@@ -97,11 +119,10 @@ public class JsonWebKeyTest {
 
     String privateJwk = testGroup.getAsJsonObject("private").toString();
     JsonObject publicJwk = testGroup.getAsJsonObject("public");
+    String verificationJwk = publicJwk == null ? privateJwk : publicJwk.toString();
 
     String jws = getFlattenedString(testCase, "jws");
     boolean expectedResult = testCase.get("result").getAsString().equals("valid");
-
-    String verificationJwk = publicJwk == null ? privateJwk : publicJwk.toString();
     boolean result = performKeysetVerification(jws, verificationJwk);
 
     if (getSuppressedTests().contains(testName)) {
@@ -109,6 +130,8 @@ public class JsonWebKeyTest {
       assertWithMessage("This test appears to be needlessly suppressed")
           .that(result)
           .isEqualTo(!expectedResult);
+      // The test fails but is suppressed.
+      TestUtil.skipTest("Suppressed test still fails");
     } else {
       assertThat(result).isEqualTo(expectedResult);
     }
@@ -145,12 +168,6 @@ public class JsonWebKeyTest {
       } catch (JoseException e) {
         throw new SecurityException("Verification key selection failed", e);
       }
-      if (usedVerificationKey == null) {
-        // The key selector would have caused this to fail but let's pretend we weren't using it.
-        // This code isn't set up to work with keysets that don't select a key (so throw).
-        usedVerificationKey = Iterables.getOnlyElement(parsedKeyset.getJsonWebKeys());
-      }
-
       verifier.setKey(usedVerificationKey.getKey());
       return verifier.verifySignature();
     } catch (Exception e) {
