278f213554184ad1f324bf51c820cec3fe5aa043
Daniel McCarney <daniel@binaryparadox.net>
2025-03-21 09:03:46 -0400

    vectorlint: lint test group IDs/count
    Lifts out the schema linting specific bits to their own func and adds
    a new func for checking that:
    
    1. The "tcId" property of each test is unique within the vector file.
    2. The "numberOfTests" property matches the number of tests found in the
      vector.

--- tools/vectorlint/main.go
+++ tools/vectorlint/main.go
@@ -16,10 +16,14 @@ import (
 	"strings"
 )
 
+var (
+	schemaDirectory    = flag.String("schemas-dir", "schemas", "directory containing schema files")
+	vectorsDirectories = flag.String("vectors-dir", "testvectors_v1,testvectors", "comma separated directories containing vector files")
+	vectorFilter       = flag.String("vector-filter", "", "only validate vector files matching the provided pattern")
+	vectorRegex        *regexp.Regexp
+)
+
 func main() {
-	schemaDirectory := flag.String("schemas-dir", "schemas", "directory containing schema files")
-	vectorsDirectories := flag.String("vectors-dir", "testvectors_v1,testvectors", "comma separated directories containing vector files")
-	vectorFilter := flag.String("vector-filter", "", "only validate vector files matching the provided pattern")
 
 	flag.Parse()
 
@@ -28,7 +32,6 @@ func main() {
 	log.Printf("reading schemas from %q\n", *schemaDirectory)
 	log.Printf("reading vectors from %q\n", vectorDirectoryParts)
 
-	var vectorRegex *regexp.Regexp
 	if *vectorFilter != "" {
 		vectorRegex = regexp.MustCompile(*vectorFilter)
 		log.Printf("filtering vectors with %q\n", *vectorFilter)
@@ -41,94 +44,21 @@ func main() {
 	}
 	schemaCompiler.AssertFormat() // Opt in to format validation.
 
-	var total, valid, invalid, noSchema, ignored int
+	var results schemaLintResults
+
 	for _, vectorDir := range vectorDirectoryParts {
-		err := filepath.WalkDir(vectorDir, func(path string, d fs.DirEntry, err error) error {
-			if err != nil {
-				return err
-			}
-
-			if d.IsDir() || !strings.HasSuffix(d.Name(), ".json") {
-				return nil
-			}
-
-			if vectorRegex != nil && !vectorRegex.MatchString(d.Name()) {
-				return nil
-			}
-
-			vectorData, err := os.ReadFile(path)
-			if err != nil {
-				return fmt.Errorf("failed to read %s: %w", path, err)
-			}
-
-			total++
-
-			var vector struct {
-				Schema string `json:"schema"`
-			}
-
-			if err := json.Unmarshal(vectorData, &vector); err != nil {
-				log.Printf("❌ %q: invalid vector JSON data: %s\n", path, err)
-				invalid++
-				return nil
-			}
-
-			if vector.Schema == "" {
-				log.Printf("❌ %q: no schema specified\n", path)
-				noSchema++
-				return nil
-			}
-
-			if missingSchemas[vector.Schema] {
-				log.Printf("⚠️ %q: ignoring missing schema %q\n", path, vector.Schema)
-				ignored++
-				return nil
-			}
-
-			schemaPath := filepath.Join(*schemaDirectory, vector.Schema)
-			if _, err := os.Stat(schemaPath); os.IsNotExist(err) {
-				log.Printf("❌ %q: referenced schema %q not found\n", path, vector.Schema)
-				invalid++
-				return nil
-			}
-
-			schema, err := schemaCompiler.Compile(schemaPath)
-			if err != nil {
-				log.Printf("❌ %q: invalid schema %q: %s\n", path, vector.Schema, err)
-				invalid++
-				return nil
-			}
-
-			var instance any
-			if err := json.Unmarshal(vectorData, &instance); err != nil {
-				log.Printf("❌ %q: invalid vector JSON data: %s\n", path, err)
-				invalid++
-				return nil
-			}
-
-			if err := schema.Validate(instance); err != nil {
-				log.Printf("❌ %q: vector doesn't validate with schema: %s\n", path, err)
-				invalid++
-				return nil
-			}
-
-			log.Printf("✅ %q: validates with %q\n", path, vector.Schema)
-			valid++
-			return nil
-		})
-		if err != nil {
-			fmt.Printf("Error walking directory: %v\n", err)
-			os.Exit(1)
+		if err := lintVectorDir(schemaCompiler, &results, vectorDir); err != nil {
+			log.Fatalf("error linting schemas: %v\n", err)
 		}
 	}
 
-	log.Printf("linted %d vector files\n", total)
-	log.Printf("valid: %d\n", valid)
-	log.Printf("invalid: %d\n", invalid)
-	log.Printf("no schema: %d\n", noSchema)
-	log.Printf("ignored: %d\n", ignored)
+	log.Printf("linted %d vector files\n", results.total)
+	log.Printf("valid: %d\n", results.valid)
+	log.Printf("invalid: %d\n", results.invalid)
+	log.Printf("no schema: %d\n", results.noSchema)
+	log.Printf("ignored: %d\n", results.ignored)
 
-	os.Exit(invalid)
+	os.Exit(results.invalid)
 }
 
 var (
@@ -231,4 +161,134 @@ func validateCurve(value any) error {
 	default:
 		return fmt.Errorf("invalid EcCurve: unknown curve name: %v", value)
 	}
+}
+
+func lintVectorDir(schemaCompiler *jsonschema.Compiler, results *schemaLintResults, vectorDir string) error {
+	err := filepath.WalkDir(vectorDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() || !strings.HasSuffix(d.Name(), ".json") {
+			return nil
+		}
+
+		if vectorRegex != nil && !vectorRegex.MatchString(d.Name()) {
+			return nil
+		}
+
+		results.total++
+
+		vectorData, err := os.ReadFile(path)
+		if err != nil {
+			return fmt.Errorf("failed to read %s: %w", path, err)
+		}
+
+		if err := lintVectorTestGroups(vectorData, path); err != nil {
+			log.Printf("❌ %q: %s\n", path, err)
+			results.invalid++
+			return nil
+		}
+
+		if err := lintVectorToSchema(schemaCompiler, vectorData, path, results); err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("error walking directory: %w", err)
+	}
+
+	return nil
+}
+
+func lintVectorTestGroups(vectorData []byte, path string) error {
+	var vector struct {
+		NumberOfTests int `json:"numberOfTests"`
+		TestGroups    []struct {
+			Tests []struct {
+				TcId int `json:"tcId"`
+			} `json:"tests"`
+		} `json:"testGroups"`
+	}
+	if err := json.Unmarshal(vectorData, &vector); err != nil {
+		return fmt.Errorf("error decoding vector JSON data for test groups: %w", err)
+	}
+
+	// Within a vector file, test case IDs must be unique.
+	testCaseIds := make(map[int]struct{})
+	for _, tg := range vector.TestGroups {
+		for _, test := range tg.Tests {
+			if _, ok := testCaseIds[test.TcId]; ok {
+				return fmt.Errorf("vector %q has duplicate tcId %d", path, test.TcId)
+			}
+			testCaseIds[test.TcId] = struct{}{}
+		}
+	}
+
+	if testCount := len(testCaseIds); testCount != vector.NumberOfTests {
+		return fmt.Errorf("vector %q declared %d tests in group, had %d", path, vector.NumberOfTests, testCount)
+	}
+
+	return nil
+}
+
+func lintVectorToSchema(schemaCompiler *jsonschema.Compiler, vectorData []byte, path string, results *schemaLintResults) error {
+	var vector struct {
+		Schema string `json:"schema"`
+	}
+
+	if err := json.Unmarshal(vectorData, &vector); err != nil {
+		log.Printf("❌ %q: invalid vector JSON data: %s\n", path, err)
+		results.invalid++
+		return nil
+	}
+
+	if vector.Schema == "" {
+		log.Printf("❌ %q: no schema specified\n", path)
+		results.noSchema++
+		return nil
+	}
+
+	if missingSchemas[vector.Schema] {
+		log.Printf("⚠️ %q: ignoring missing schema %q\n", path, vector.Schema)
+		results.ignored++
+		return nil
+	}
+
+	schemaPath := filepath.Join(*schemaDirectory, vector.Schema)
+	if _, err := os.Stat(schemaPath); os.IsNotExist(err) {
+		log.Printf("❌ %q: referenced schema %q not found\n", path, vector.Schema)
+		results.invalid++
+		return nil
+	}
+
+	schema, err := schemaCompiler.Compile(schemaPath)
+	if err != nil {
+		log.Printf("❌ %q: invalid schema %q: %s\n", path, vector.Schema, err)
+		results.invalid++
+		return nil
+	}
+
+	var instance any
+	if err := json.Unmarshal(vectorData, &instance); err != nil {
+		log.Printf("❌ %q: invalid vector JSON data: %s\n", path, err)
+		results.invalid++
+		return nil
+	}
+
+	if err := schema.Validate(instance); err != nil {
+		log.Printf("❌ %q: vector doesn't validate with schema: %s\n", path, err)
+		results.invalid++
+		return nil
+	}
+
+	log.Printf("✅ %q: validates with %q\n", path, vector.Schema)
+	results.valid++
+	return nil
+}
+
+type schemaLintResults struct {
+	total, valid, invalid, noSchema, ignored int
 }
